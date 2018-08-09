f9e78aa92d60f88a060d235f53f8a0fa4e40269a
EGH0158 <EGH0158@EGHWS0046.enighana.intranet>
2018-08-09 20:58:03 +0000

    MYJOYNEWS INITIAL SCRAP WORKS

--- src/main/java/Processor/MyJoyOnlinePageProcessor.java
+++ src/main/java/Processor/MyJoyOnlinePageProcessor.java
@@ -21,32 +21,53 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * @author Godfred O Sabbih
  */
 public class MyJoyOnlinePageProcessor implements PageProcessor {
-
+    
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
     private Document doc;
-
+    
     @Override
     public void process(Page page) {
         JoyOnlineModel model = new JoyOnlineModel();
         page.addTargetRequests(page.getHtml().links().regex("(https://www.myjoyonline\\.com/\\w+/\\d+/\\S+)").all());
         doc = Jsoup.parse(page.getHtml().get());
-        Elements allDivs=doc.getElementsByTag("div");
+        Elements allDivs = doc.getElementsByTag("div");
 //         System.out.println("RESULT = "+allDivs.text());
-        for(Element e:allDivs)
-        {
+        for (Element e : allDivs) {
 //             System.out.println("RESULT = "+e.text());
-            if(e.className().equals("article-heading")){
-               
+
+            if (e.className().equals("article-heading")) {
+                
                 Elements headingElements = e.getAllElements();
-                 for(Element hE:headingElements){
-                     if(hE.tagName().equals("h1")){
-                         model.setArticleheadline(hE.text());
+                for (Element hE : headingElements) {
+                    if (hE.tagName().equals("h1")) {
+                        model.setArticleheadline(hE.text());
 //                         System.out.println("RESULT = "+hE.text());
-                     }
-                     
-                 }
+                    }
+                    if (hE.tagName().equals("div")) {
+                        model.setDateofPublishing(hE.text());
+//                         System.out.println("RESULT = "+hE.text());
+                    }
+                    
+                }
+            }
+            if (e.className().equals("article-image")) {
+                
+                Elements articleElements = e.getAllElements();
+                for (Element hE : articleElements) {
+                    if (hE.tagName().equals("img")) {
+                        model.setCaptionImageUrl(hE.attr("src"));
+//                         System.out.println("RESULT = "+hE.text());
+                    }
+                    
+                }
+            }
+            if (e.className().equals("article-text")) {
+                
+                model.setArticleContent(e.text());
             }
         }
+        model.setSourceUrl(page.getUrl().get());
+        
         if (model.getArticleheadline() == null) {
             page.setSkip(true);
         }
@@ -54,11 +75,11 @@ public class MyJoyOnlinePageProcessor implements PageProcessor {
 //        page.putField(Constants.CITINEWS_REPO_KEY, page.getHtml().xpath("//article/header/h1[@itemprop='headline']/text()"));
 
     }
-
+    
     @Override
     public Site getSite() {
         return site;
-
+        
     }
-
+    
 }
